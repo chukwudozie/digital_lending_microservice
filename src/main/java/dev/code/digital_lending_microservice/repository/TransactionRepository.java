8533cb5e58ac435b907d7d4cb2ff528d720f9604
chukwudozie <emkychuks062@gmail.com>
2022-08-28 17:54:38 +0100

    add annotation on transaction repository

--- src/main/java/dev/code/digital_lending_microservice/repository/TransactionRepository.java
+++ src/main/java/dev/code/digital_lending_microservice/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package dev.code.digital_lending_microservice.repository;
 
 import dev.code.digital_lending_microservice.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
