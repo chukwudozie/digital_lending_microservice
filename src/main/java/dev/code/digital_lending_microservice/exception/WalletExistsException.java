f081e1bd7ff5b4770ccdeab2ed32a27a37360175
chukwudozie <emkychuks062@gmail.com>
2022-08-30 17:23:23 +0100

    update git ignore

---
+++ src/main/java/dev/code/digital_lending_microservice/exception/WalletExistsException.java
@@ -0,0 +1,6 @@
+package dev.code.digital_lending_microservice.exception;
+
+public class WalletExistsException extends Throwable {
+    public WalletExistsException(String wallet_already_exists) {
+    }
+}
